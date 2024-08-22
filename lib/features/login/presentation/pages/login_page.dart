8a3082b464a676f04126235f630522f417473d34
Gustavo Herrero Nunes <51753426+GustavoHerreroNunes@users.noreply.github.com>
2024-08-22 00:19:18 +0000

    Add: DefaultTextField widget

--- lib/features/login/presentation/pages/login_page.dart
+++ lib/features/login/presentation/pages/login_page.dart
@@ -1,8 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:maps_route_app/features/widgets/default_text_field_widget.dart';
+import 'dart:html';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _myTxbController = TextEditingController();
+
+  @override void initState() {
+    super.initState();
+    
+    _myTxbController.addListener(() {
+      window.console.info("[_myTxbController.text]");
+      window.console.info(_myTxbController.text);
+    });
+  }
+
+  @override
+  void dispose() {
+    _myTxbController.dispose();
+
+    super.dispose();
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return const Text("Login Page");
+    return Material(
+      child: DefaultTextField(
+        "My Text Field", 
+        InputType.password, 
+        _myTxbController
+      ),
+    );
   }
 }
