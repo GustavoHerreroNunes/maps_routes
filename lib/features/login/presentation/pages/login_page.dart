71f765390f0ab67215b296377bbfa4a7e37d896e
Gustavo Herrero Nunes <51753426+GustavoHerreroNunes@users.noreply.github.com>
2024-08-22 00:45:51 +0000

    Add: First widgets on the Login Page

--- lib/features/login/presentation/pages/login_page.dart
+++ lib/features/login/presentation/pages/login_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:maps_route_app/constants/routes_const.dart';
 import 'package:maps_route_app/features/widgets/default_text_field_widget.dart';
 import 'dart:html';
+
+import 'package:maps_route_app/main.dart';
 
 class LoginPage extends StatefulWidget {
 
@@ -9,32 +12,76 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final _myTxbController = TextEditingController();
+  final _txbEmailController = TextEditingController();
+  final _txbPassController = TextEditingController();
 
   @override void initState() {
     super.initState();
-    
-    _myTxbController.addListener(() {
-      window.console.info("[_myTxbController.text]");
-      window.console.info(_myTxbController.text);
+
+    _txbEmailController.addListener(() {
+      window.console.info("[_txbEmail]: ${_txbEmailController.text}");
+    });
+
+    _txbPassController.addListener(() {
+      window.console.info("[_txbPass]: ${_txbPassController.text}");
     });
   }
 
   @override
   void dispose() {
-    _myTxbController.dispose();
+    _txbEmailController.dispose();
+    _txbPassController.dispose();
 
     super.dispose();
   }
   
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: DefaultTextField(
-        "My Text Field", 
-        InputType.password, 
-        _myTxbController
-      ),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Bem-vinde!",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w600,
+              ),
+              textDirection: TextDirection.ltr,
+            ),
+            Row(
+              children: [
+                const Text("Entre ou"),
+                TextButton(
+                  child: const Text("cadastre-se"),
+                  onPressed: () {
+                    MapsRouteApp.navigatorKey.currentState
+                      ?.pushNamed(RoutesConst.signup);
+                  }
+                )
+              ],
+            ),
+            _txbEmail(_txbEmailController),
+            _txbPass(_txbPassController)
+          ],
+        )
+      )
     );
+  }
+
+  Widget _txbEmail(dynamic controller){
+    const String label = "Email";
+    const InputType inputType = InputType.text;
+
+    return DefaultTextField(label, inputType, controller);
+  }
+
+  Widget _txbPass(dynamic controller){
+    const String label = "Senha";
+    const InputType inputType = InputType.password;
+
+    return DefaultTextField(label, inputType, controller);
   }
 }
